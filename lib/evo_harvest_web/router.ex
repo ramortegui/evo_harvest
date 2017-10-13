c958edc27c4e6dbee6aa0d3192a208a2233cc086
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-10-12 21:41:17 -0600

    Add first query, and update the Endpoint routes.

--- lib/evo_harvest_web/router.ex
+++ lib/evo_harvest_web/router.ex
@@ -1,26 +1,17 @@
 defmodule EvoHarvestWeb.Router do
   use EvoHarvestWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", EvoHarvestWeb do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
+  scope "/" do
+    pipe_through :api
+    forward "/api", Absinthe.Plug,
+      schema: EvoHarvestWeb.Schema
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: EvoHarvestWeb.Schema,
+      interface: :simple
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", EvoHarvestWeb do
-  #   pipe_through :api
-  # end
 end
--- lib/evo_harvest_web/schema.ex
+++ lib/evo_harvest_web/schema.ex
@@ -1,11 +1,12 @@
 defmodule EvoHarvestWeb.Schema do
   use Absinthe.Schema
-  alias EvoHarvest.Repo;
-  alis 
-
+  alias EvoHarvest.Repo
   query do
     field :companies, list_of(:company)
-    resolve fn _,_,_ -> { :ok, Repo.all(Repo.Company) }
+    resolve
+      fn _,_,_ ->
+        { :ok, Repo.all(Repo.Company) }
+    end
   end
 
   object :company do
