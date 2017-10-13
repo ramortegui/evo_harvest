2ee6a81b701357656400fb2adbf36d3bb4de543b
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-10-12 20:24:15 -0600

    Add absinthe library.

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule EvoHarvest.Mixfile do
   def application do
     [
       mod: {EvoHarvest.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :absinthe]
     ]
   end
 
@@ -40,7 +40,8 @@ defmodule EvoHarvest.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:absinthe, "~> 1.3.2" }
     ]
   end
 
